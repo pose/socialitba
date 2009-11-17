17d83d06b5e9a58b17198385a0ce5e277eab4718
catalanojuan <catalanojuan@df28040a-ab18-11de-b87c-2b1544cb6d8d>
2009-11-17 03:24:06 +0000

    Subo varios avances sobre el TP de FB.

--- trunk/facebook/dmessages.php
+++ trunk/facebook/dmessages.php
@@ -1,8 +1,39 @@
 <?php
-/*
- * Created on Oct 2, 2009
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
+	require_once 'facebook/facebook.php';
+	require_once('twitter/twitterOAuth.php');
+	include 'twitter/EpiCurl.php';
+	include 'twitter/EpiOAuth.php';
+	include 'twitter/EpiTwitter.php';
+	include 'secret.php';
+	
+	$facebook = new Facebook($appapikey, $appsecret);
+	$user_id = $facebook->require_login();
+	$db = mysql_connect('localhost','fbapp','lalala') or die("Database error");
+	
+	mysql_select_db('test', $db);
+		
+	$query = sprintf("SELECT * FROM user WHERE fb_uid=%d", mysql_real_escape_string($user_id) );
+	
+	$result = mysql_query($query);
+
+	$row = mysql_fetch_array($result, MYSQL_ASSOC);
+	
+	$oauth_token = $row['oauth_token'];
+	
+	$oauth_token_secret = $row['oauth_token_secret'];
+	
+	$twitterObj = new EpiTwitter($consumer_key, $consumer_secret);
+
+	$twitterObj->setToken($oauth_token, $oauth_token_secret);
+	
+	$twitterInfo= $twitterObj->get_direct_messages();
+	
+	$html = "<link rel='stylesheet' type='text/css' href='http://190.16.39.171/style.css' />" .
+			"<fb:header icon='false'>My Direct Messages</fb:header>";
+			
+	foreach ( $twitterInfo->response as $st ){
+		$html .= sprintf (  "<br/> <div class='fbgreybox'> %s - %s </div><br/>", $st['sender']['name'], $st['text'] );
+	}
+	
+	echo $html;
 ?>
